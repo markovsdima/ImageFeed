50eb1c854286396c6d36d2ed52ceef0bdb8be070
markovsdima <markovskii-dmitrii@yandex.ru>
2023-11-26 12:35:01 +0300

    Сверстан экран профиля пользователя

---
+++ ImageFeed/ProfileScreen/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitry Markovskiy on 26.11.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
