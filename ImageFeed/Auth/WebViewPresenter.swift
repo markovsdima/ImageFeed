7da1db66061a2a3a6a6f42c4f3dc6fd8ee018c02
markovsdima <markovskii-dmitrii@yandex.ru>
2024-02-04 01:25:32 +0300

    WebViewUnitTests

--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -26,16 +26,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     
     // MARK: - Public Methods
     func viewDidLoad() {
-//        var urlComponents = URLComponents(string: Constants.unsplashAuthorizeURLString)!
-//        urlComponents.queryItems = [
-//            URLQueryItem(name: "client_id", value: Constants.accessKey),
-//            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-//            URLQueryItem(name: "response_type", value: "code"),
-//            URLQueryItem(name: "scope", value: Constants.accessScope)
-//        ]
-//        
-//        let url = urlComponents.url!
-//        let request = URLRequest(url: url)
         let request = authHelper.authRequest()
         view?.load(request: request)
         didUpdateProgressValue(0)
